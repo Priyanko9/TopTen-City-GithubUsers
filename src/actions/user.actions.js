c301917e3e171716e2240c591cc57584f157849f
Priyanko Das <priyankodas74@gmail.com>
2019-12-09 18:57:46 +0530

    Performance Improvement

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -13,26 +13,27 @@ export function getUsers(city) {
         let results=await fetch('https://api.github.com/search/users?q=location:'+city+'&sort:followers',config);
         let usersObject=await results.json();
         let topTenUsers=usersObject.items && usersObject.items.slice(0,10);
-        let topTenUsersInfo=[];
         let errorMsg;
-        for(let i=0;i<topTenUsers.length;i++){
-                let eachUserResult=await fetch(topTenUsers[i].url,config);
-                let userObject=await eachUserResult.json();
-                if(eachUserResult.status===403){
-                    errorMsg=userObject.message;
-                    topTenUsersInfo.length=0;
-                    break;
-                } else {
-                    topTenUsersInfo.push(userObject);
+        Promise.all(topTenUsers.map(ele=>fetch(ele.url,config)))
+        .then(data=>Promise.all(data.map(ele=>ele.json())))
+        .then(finalResult=>{
+                for(let i=0;i<finalResult.length;i++){
+                    if(finalResult[i].status===403){
+                        errorMsg=finalResult[i].message;
+                        break;
+                    }
                 }
-                
-        }
-        if(!errorMsg){
-            dispatch({ type: userConstants.GET_USERS, users:topTenUsersInfo });
-        } else{
-            dispatch({ type: errorConstants.ERROR, msg:errorMsg } );
-        }
-        dispatch({ type: userConstants.FINISH_REQUEST });
+                if(errorMsg){
+                    dispatch({ type: errorConstants.ERROR, msg:errorMsg } );
+                } else {
+                    dispatch({ type: userConstants.GET_USERS, users:finalResult });
+                }
+                dispatch({ type: userConstants.FINISH_REQUEST });
+        })
+        .catch(err=>{
+            dispatch({ type: errorConstants.ERROR, msg:err } );
+            dispatch({ type: userConstants.FINISH_REQUEST });
+        })
     }
 }
 
